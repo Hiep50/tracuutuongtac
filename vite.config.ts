c6465bc85b942d6d09df3ee1c351c0286129e2ae
Hiep50 <nhhiep0416@gmail.com>
2026-06-15 15:46:16 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/tracuutuongtac/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
